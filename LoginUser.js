d32d19e05e468abfc4665e3a8b6856fd6bd44065
NikhilDurgadas2244 <91554835+NikhilDurgadas2244@users.noreply.github.com>
2022-03-23 15:13:02 +0000

    login authentification form & JSON trial

--- LoginUser.js
+++ LoginUser.js
@@ -1,13 +1,24 @@
-function checkUser(){
-    const username = document.getElementById("email1").value;
+function checkUser() {
+    const email = document.getElementById("email1").value;
     const password = document.getElementById("password1").value;
-    console.log(username);
-    console.log(password);
-    console.log("aaaaaa");
-    if(username === "admin@admin.com" && password === "admin"){
-        alert("You have logged in as Administrator");
-    }
-    else{
-        alert("Log in error, please try again with another email and password");
-    }
+
+    $.ajax({
+        type: "GET",
+        url : "Users.json",
+        success:function(response)
+        {
+            console.log(response);
+            for(i = 0; i < response.length; i++){
+                if(email === response[i].address && password === response[i].password){
+                    window.alert("You have logged in as " + response[i].name);
+                    window.location.href="index.html";
+                    break;
+                }
+                else{
+                    alert("Log in error, please try again with another email and password");
+                    break;
+                }
+            }
+        }
+    });
 }

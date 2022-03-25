ea43db3f69a5bfb4a496d8781a479d72ea72b041
raulmat19 <91469476+raulmat19@users.noreply.github.com>
2022-03-25 14:09:29 +0000

    Forms v1 | Login and Sign Up

--- scripts/LoginUser.js
+++ scripts/LoginUser.js
@@ -1,7 +1,7 @@
 function checkUser() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-
+    let fail;
     $.ajax({
         type: "GET",
         url : "data/Users.json",
@@ -12,12 +12,16 @@ function checkUser() {
                 if(email === response[i].address && password === response[i].password){
                     window.alert("You have logged in as " + response[i].name);
                     window.location.href="index.html";
+                    fail = true;
                     break;
                 }
                 else{
-                    alert("Log in error, please try again with another email and password");
-                    break;
+                    fail = false;
                 }
+            }
+
+            if (!fail){
+                alert("Log in error, please try again with another email and password");
             }
         }
     });
